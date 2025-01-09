8dac727396898905aad3ca830a8c057d8cd733e7
PHIDIAS <phidias@aperx.org>
2025-01-10 00:11:45 +0900

    .

--- exp_legacy/module/modules/control/vlayer.lua
+++ exp_legacy/module/modules/control/vlayer.lua
@@ -379,7 +379,7 @@ local function handle_input_interfaces()
                     elseif prototypes.quality[v.quality].level > 0 and v.count >= 10 then
                         local batch = math.floor(v.count / 10)
                         count_deduct = batch * 10
-                        count_add = batch * (7 + (prototypes.quality[v.quality].level * 3))
+                        count_add = batch * (10 + (prototypes.quality[v.quality].level * 3))
                     end
 
                     if count_deduct and count_add then
