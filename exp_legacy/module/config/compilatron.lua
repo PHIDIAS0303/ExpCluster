f1868e883cc1913674ace6a8666e0b5a9979b780
PHIDIAS <phidias@aperx.org>
2025-01-09 14:42:32 +0900

    .

--- exp_legacy/module/config/compilatron.lua
+++ exp_legacy/module/config/compilatron.lua
@@ -9,9 +9,7 @@ return {
     messages = { --- @setting messages the messages that each one will say, must be same name as its location
         ["Spawn"] = {
             { "info.website" },
-            { "info.cloud" },
             { "info.github" },
-            { "info.graph" },
             { "info.read-readme" },
             { "info.softmod" },
             { "info.custom-commands" },
