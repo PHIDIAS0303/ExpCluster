02561e36c64ad321a9d649870ea6c152482753d9
PHIDIAS <phidias@aperx.org>
2025-02-12 19:26:12 +0900

    .

--- exp_legacy/module/modules/addons/miner.lua
+++ exp_legacy/module/modules/addons/miner.lua
@@ -124,8 +124,8 @@ local function miner_check(entity)
             for i = 1, #e.fluidbox do
                 for _, p in pairs(e.fluidbox.get_pipe_connections(i)) do
                     if p.flow_direction == "input" or p.flow_direction == "input-output" then
-                        for x = 1, p.position.x do
-                            for y = 1, p.position.y do
+                        for x = 1, p.target_position.x do
+                            for y = 1, p.target_position.y do
                                 table.insert(pipe_build, { x = x, y = y })
                             end
                         end
