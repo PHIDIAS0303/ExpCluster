d344131a7484357cddfb58626f365f3e1b7b9051
PHIDIAS <phidias@aperx.org>
2025-04-16 17:44:52 +0900

    .

--- exp_scenario/module/commands/research.lua
+++ exp_scenario/module/commands/research.lua
@@ -19,11 +19,21 @@ Storage.register(research, function(tbl)
     research = tbl
 end)
 
+for _, mod_name in ipairs(config.mod_set_lookup) do
+    if script.active_mods[mod_name] then
+        config.mod_set = mod_name
+        break
+    end
+end
+
+if script.active_mods["PHI-CL"] and settings.startup["PHI-VP"] and settings.startup["PHI-VP-MAIN"] then
+    config.mod_set = "base"
+end
+
 --- @param force LuaForce
 --- @param silent boolean True when no message should be printed
 function module.res_queue(force, silent)
     local res_q = force.research_queue
-    rcon.print(config.mod_set)
     local res = force.technologies[config.bonus_inventory.res[config.mod_set].name]
 
     if #res_q < config.queue_amount then
